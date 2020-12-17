1c52d9d9ebaba68db5fb8abf327c4d6f6f420186
fazelastblood <74438959+fazelastblood@users.noreply.github.com>
2020-12-17 14:03:56 -0600

    Add files via upload

---
+++ MainScene.js
@@ -0,0 +1,17 @@
+export default class MainScene extends Phaser.Scene {
+    constructor() {
+        super("MainScene");
+    }
+
+    preload() {
+        console.log("pre");
+    }
+
+    create() {
+        console.log("create");
+    }
+
+    update() {
+        console.log("update");
+    }
+}
